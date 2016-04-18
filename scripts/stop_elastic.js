108c7bcef4d11de371dfbb9395af5b228e46258e
Maximilian Klein <mklein@welfenlab.de>
2016-04-18 16:10:39 +0200

    (chore) Detect style problems on CI.

--- scripts/stop_elastic.js
+++ scripts/stop_elastic.js
@@ -8,22 +8,24 @@ import fs from 'fs'
 import isCi from 'is-ci'
 
 export function killElastic (callback) {
-  var contents = fs.readFileSync(__dirname + '/.download/running.pid', 'utf8')
-  processExists(Number(contents)).then(exists => {
-    if (exists) {
-      console.log('elastic search already running with PID, ' + chalk.red('killing it: '), contents)
-      kill(Number(contents), 'SIGTERM').then(() => {
-        console.log(chalk.yellow('removing old data at ' + __dirname + '/.download/elastic/data/elasticsearch'))
-        fs.unlinkSync(__dirname + '/.download/running.pid')
-        rimraf(__dirname + '/.download/elastic/data/elasticsearch', () => {
-          callback()
+  try {
+    var contents = fs.readFileSync(__dirname + '/.download/running.pid', 'utf8')
+    processExists(Number(contents)).then(exists => {
+      if (exists) {
+        console.log('elastic search already running with PID, ' + chalk.red('killing it: '), contents)
+        kill(Number(contents), 'SIGTERM').then(() => {
+          console.log(chalk.yellow('removing old data at ' + __dirname + '/.download/elastic/data/elasticsearch'))
+          fs.unlinkSync(__dirname + '/.download/running.pid')
+          rimraf(__dirname + '/.download/elastic/data/elasticsearch', () => {
+            callback()
+          })
         })
-      })
-    } else {
-      console.log('elastic search not running [old PID: ', contents, ']')
-      callback()
-    }
-  })
+      } else {
+        console.log('elastic search not running [old PID: ', contents, ']')
+        callback()
+      }
+    })
+  } catch (e) {}
 }
 
 if (!isCi && require.main === module) {
